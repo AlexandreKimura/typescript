b99c427e2ae4f9a1423a4b795cf1ef419e316f0d
Alexandre Fukano <alexandremkimura@hotmail.com>
2023-03-07 00:31:19 -0300

    fix some problems

--- src/app.ts
+++ src/app.ts
@@ -119,6 +119,36 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 
 }
 
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+
+  private project: Project
+
+  constructor(hostId: string, project: Project) {
+    super('single-project', hostId, false, project.id)
+    this.project = project
+
+    this.configure()
+    this.renderContent()
+  }
+
+  get persons() {
+    if (this.project.people === 1) {
+      return '1 persion'
+    }
+    return `${this.project.people} persons`
+  }
+
+  configure(): void {
+
+  }
+
+  renderContent(): void {
+    this.element.querySelector('h2')!.textContent = this.project.title
+    this.element.querySelector('h3')!.textContent = this.persons + ' assigned'
+    this.element.querySelector('p')!.textContent = this.project.description
+  }
+}
+
 class ProjectList extends Component<HTMLDivElement, HTMLElement> {
   assignedProjects: Project[]
 
@@ -154,9 +184,7 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
     const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement
     listEl.innerHTML = ''
     for (const projectItem of this.assignedProjects) {
-      const listItem = document.createElement('li')
-      listItem.textContent = projectItem.title
-      listEl.appendChild(listItem)
+      new ProjectItem(this.element.querySelector('ul')!.id, projectItem)
     }
   }
 }
